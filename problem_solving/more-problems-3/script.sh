303a2c70e2b9343ec6cb05b59b6dc9bb3eb294b8
MD. SIAM AFROZ TANMOY <mdsiamafroztanmoy@gmail.com>
2026-05-04 20:31:41 +0600

    Update script.sh

--- problem_solving/more-problems-3/script.sh
+++ problem_solving/more-problems-3/script.sh
@@ -1,1 +1,18 @@
-sd
+#!/bin/bash
+
+
+for file in *
+
+do
+    if [ -f "$file" ]; then
+        echo "$file" | grep -q "[0-9]"
+
+        if [ $? -eq 0 ]; then
+            rm "$file"
+        fi
+
+    
+    fi
+
+
+done
