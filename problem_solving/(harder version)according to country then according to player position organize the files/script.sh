112dde29cc8576050b768a1d3e7475b2c30f6708
MD. SIAM AFROZ TANMOY <mdsiamafroztanmoy@gmail.com>
2026-05-04 18:43:55 +0600

    Sort files by country and player position
    Organize text files into directories based on country and player position.

--- problem_solving/(harder version)according to country then according to player position organize the files/script.sh
+++ problem_solving/(harder version)according to country then according to player position organize the files/script.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
 
-for file in 
+find . -type f -name "*.txt" | while read file
+
+do
+    country_name=$(head -2 "$file"|tail -1)
+    position=$(head -4 "$file"|tail -1)
+
+    if [ ! -f "$country_name" ]
+        then 
+            mkdir "$country_name"
+    fi    
+
+    if [ ! -f "$country_name/$position" ]
+        then 
+            mkdir "$country_name/$position"
+    fi  
+
+    mv "$file" "$country_name/$position" 
+
+
+
+
+done
+find . -type d -empty -delete
