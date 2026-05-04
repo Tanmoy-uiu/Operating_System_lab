89c645717b722d0bc01181160489c612826e4716
MD. SIAM AFROZ TANMOY <mdsiamafroztanmoy@gmail.com>
2026-05-04 20:06:57 +0600

    Update script.sh

--- problem_solving/more-problem-1/script.sh
+++ problem_solving/more-problem-1/script.sh
@@ -1,1 +1,39 @@
-sd
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "usage: $0 filename1 filename2 ..."
+    exit
+fi
+for file in "$@"
+do
+    if [ ! -e "$file" ]
+        then
+            echo "$file does not exits"
+    
+    elif [ ! -f "$file" ]
+        then
+            echo "$file is not a regular file"
+   
+    else
+        if [ -x "$file" ]
+            then
+                echo "$file is currently executable"
+        
+        else
+            echo "$file is not currently executable"
+        fi
+        echo "$file's executable permission is now changing"
+
+        chmod -x "$file"
+        ls -l "$file"
+
+        
+
+    fi
+
+    
+
+
+
+
+done
