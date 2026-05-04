f68d242ab5ab8a5224c860aeaeb4a444fbbb91b8
MD. SIAM AFROZ TANMOY <mdsiamafroztanmoy@gmail.com>
2026-05-04 19:15:23 +0600

    Update script.sh

--- problem_solving/more-problems-2/script.sh
+++ problem_solving/more-problems-2/script.sh
@@ -1,1 +1,15 @@
-sd
+#!/bin/bash
+
+line=$1
+pattern=$2
+for file in *.txt
+do
+    line_count=$(head -"$line" "$file" |tail -1| grep "$pattern"|wc -l)
+
+    if [ "$line_count" -gt 0 ]
+        then 
+            rm "$file"
+    fi    
+
+
+done
